1af45aa24d288e5ba6ee9507ebe63f7b8d399f54
Rustam <rstm.sf@gmail.com>
2021-11-05 18:31:19 +0300

    Update infra (#7)

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@
 
 const path = require('path');
 
-/**@type {import('webpack').Configuration}*/
-const config = {
-  infrastructureLogging: {
-    level: 'log',
-  },
+//@ts-check
+/** @typedef {import('webpack').Configuration} WebpackConfig **/
+
+/** @type WebpackConfig */
+const extensionConfig = {
   target: 'node', // vscode extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
   mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
@@ -19,9 +19,9 @@ const config = {
     filename: 'extension.js',
     libraryTarget: 'commonjs2'
   },
-  devtool: 'nosources-source-map',
   externals: {
     vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
@@ -39,6 +39,10 @@ const config = {
         ]
       }
     ]
-  }
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
 };
-module.exports = config;
+module.exports = [ extensionConfig ];
