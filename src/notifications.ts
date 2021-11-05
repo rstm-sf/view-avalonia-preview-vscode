c7a24c2f2388551bf56d3cfffe76a62b93803211
Rustam <rstm.sf@gmail.com>
2021-11-06 00:54:28 +0300

    Run previewer by LSP (#8)
    * Run previewer by LSP
    
    * Bump version extension
    
    * Update deps

--- src/notifications.ts
+++ src/notifications.ts
@@ -1,27 +1,36 @@
-import * as vscode from 'vscode';
 import { NotificationType } from 'vscode-jsonrpc';
 import { LanguageClient } from 'vscode-languageclient/node';
 
-export interface PreviewParameters {
-    avaloniaPreviewPath: string;
-    targetPath: string;
-    projectDepsFilePath: string;
-    projectRuntimeConfigFilePath: string;
-    xamlFilePath: string;
+export interface StartPreviewMessage {
+    htmlUrl: string;
+    xamlFilePath: string
 }
 
 export namespace NotificationTypes {
-    export const preview = new NotificationType<PreviewParameters>('view/avalonia/preview');
+    export const startPreview = new NotificationType<StartPreviewMessage>('view/avalonia/start-preview');
+    export const stopPreview = new NotificationType<void>('view/avalonia/stop-preview');
 }
 
-interface PreviewRunnerCallback {
-    (parameters: PreviewParameters): void
+interface StartPreviewCallback {
+    (parameters: StartPreviewMessage): void
 }
 
-export function handlePreviewNotifications(languageClient: LanguageClient, callBack: PreviewRunnerCallback): void {
+interface StopPreviewCallback {
+    (): void
+}
+
+export function handleStartPreviewNotifications(languageClient: LanguageClient, callBack: StartPreviewCallback): void {
     languageClient.onReady().then(() => {
-        languageClient.onNotification(NotificationTypes.preview, (notification: PreviewParameters) => {
+        languageClient.onNotification(NotificationTypes.startPreview, (notification: StartPreviewMessage) => {
             callBack(notification);
+        });
+    });
+}
+
+export function handleStopPreviewNotifications(languageClient: LanguageClient, callBack: StopPreviewCallback): void {
+    languageClient.onReady().then(() => {
+        languageClient.onNotification(NotificationTypes.stopPreview, () => {
+            callBack();
         });
     });
 }
