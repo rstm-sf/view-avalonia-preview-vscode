bef75371d46b00579143269e3443271e707fdfde
Rustam Sayfutdinov <rstm.sf@gmail.com>
2021-11-06 23:24:26 +0300

    Supports Content-Security-Policy

--- src/AvaloniaPreviewProvider.ts
+++ src/AvaloniaPreviewProvider.ts
@@ -6,6 +6,10 @@ export class AvaloniaPreviewProvider implements vscode.WebviewViewProvider {
 
     private _view?: vscode.WebviewView;
 
+    constructor(
+        private readonly _extensionUri: vscode.Uri,
+    ) { }
+
     resolveWebviewView(
         webviewView: vscode.WebviewView,
         context: vscode.WebviewViewResolveContext<unknown>,
@@ -18,9 +22,31 @@ export class AvaloniaPreviewProvider implements vscode.WebviewViewProvider {
         };
     }
 
-    public setHtmlToWebview(html: string) {
+    public setHtmlToWebview(htmlUrl: string) {
         if (this._view) {
-            this._view.webview.html = html;
+            this._view.webview.html = htmlUrl ? this._getHtmlForWebview(this._view.webview, htmlUrl) : '';
         }
+    }
+
+    private _getHtmlForWebview(webview: vscode.Webview, htmlUrl: string): string {
+        const stylePreviewUri = webview.asWebviewUri(vscode.Uri.joinPath(this._extensionUri, 'media', 'preview.css'));
+
+        return `
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta http-equiv="Content-Security-Policy"
+                          content="default-src 'none'; frame-src ${htmlUrl}; style-src ${webview.cspSource}">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                    <title>Avalonia Previewer</title>
+
+                    <link href="${stylePreviewUri}" rel="stylesheet">
+                </head>
+                <body>
+                    <iframe src="${htmlUrl}" />
+                </body>
+            </html>`
     }
 }
